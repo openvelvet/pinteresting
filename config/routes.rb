a60b8e5c819f7c3530a26e197ff12ba4fa094571
Omar Amiri <omar@openvelvet.com>
2013-12-11 18:05:48 -0800

    Home Page and About Us Customization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root 'pages#home'
   get "about" => "pages#about" # creates about_path
+  get "how_it_works" => "pages#how_it_works"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
